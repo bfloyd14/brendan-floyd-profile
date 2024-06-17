e7dc742dff5ad9fc33d69c49776f6457e445eb46
bfloyd14 <brendanflo1425@gmail.com>
2024-06-17 14:39:09 -0400

    add resume to resume page and made it clickable

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,8 +1,9 @@
 //npm module
 import { Link } from "react-router-dom"
 
-//icon
+//assets
 import resumeIcon from '../../assets/icon/resume-7.svg'
+import resume from '../../assets/images/brendan-resume.pdf'
 
 //css
 import styles from './Resume.module.css'
@@ -12,7 +13,7 @@ const Resume = () => {
     <>
       <div className={styles.container}>
         <h1>Brendan's Resume</h1>
-        <a href="resume-icon"><img id='resume' src={resumeIcon} alt="a cool resume icon" /></a>
+        <a href={resume} target="blank"><img id='resume' src={resumeIcon} alt="a cool resume icon" /></a>
       </div>
     </>
     
