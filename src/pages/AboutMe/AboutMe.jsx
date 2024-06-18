b38dcdd4a9e80a31c19492179e259dd8408789eb
bfloyd14 <brendanflo1425@gmail.com>
2024-06-18 12:00:56 -0400

    add animation to resume page and hover color to nav bar.

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -8,7 +8,14 @@ const AboutMe = () => {
       <div className={styles.container}>
         <div className={styles.info}>
         <h1>About Me</h1>
-        <p>I am Brendan, a fullstack software engineer, former sales specialist with 12+ years of sales and managment experience.</p>
+        <p>Hi, I am Brendan Floyd, I have recently </p>
+        <p>changed my career of 12 years of sales </p>
+          <p>
+          and management in the Beverage Alcohol 
+          </p>
+          <p>
+          space to Software Engineering.
+          </p>
         </div>
       </div>
     </>
