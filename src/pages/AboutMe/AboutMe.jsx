44f36105d09befa2bc25ed09b3f0d28094bbb659
bfloyd14 <brendanflo1425@gmail.com>
2024-06-18 16:59:15 -0400

    fix css typewriting animation.

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -8,12 +8,12 @@ const AboutMe = () => {
       <div className={styles.container}>
         <div className={styles.info}>
         <h1>About Me</h1>
-        <p>Hi, I am Brendan Floyd, I have recently </p>
-        <p>changed my career of 12 years of sales </p>
-          <p>
+        <p className={styles.p1}>Hi, I am Brendan Floyd, I have recently </p>
+        <p className={styles.p2}>changed my career of 12 years of sales </p>
+          <p className={styles.p3}>
           and management in the Beverage Alcohol 
           </p>
-          <p>
+          <p className={styles.p4}>
           space to Software Engineering.
           </p>
         </div>
