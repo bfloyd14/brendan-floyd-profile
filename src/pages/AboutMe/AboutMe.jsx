5ed965289663f5af69e2b3c51d2746edb76a0363
bfloyd14 <brendanflo1425@gmail.com>
2024-06-19 09:53:07 -0400

    add technologies to about me page.

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -17,6 +17,20 @@ const AboutMe = () => {
           space to Software Engineering.
           </p>
         </div>
+        <div className={styles.info}>
+          <div className={styles.title}>
+            <h1>Technologies</h1>
+          </div>
+          <div className={styles.technology}>
+          <a href='#'><img src="https://img.shields.io/badge/-HTML5-E34F26?style=flat-square&logo=html5&logoColor=white"/></a>
+          <a href='#'><img src='https://img.shields.io/badge/-MongoDB-white?style=flat-square&logo=mongodb'/></a>
+          <a href='#'><img src='https://img.shields.io/badge/-Django-092E20?style=flat-square&logo=django'/></a><a href='#'><img src="https://img.shields.io/badge/-CSS3-1572B6?style=flat-square&logo=css3"/></a>
+          <a href='#'><img src="https://img.shields.io/badge/-JavaScript-F7DF1E?style=flat-square&logo=javascript&logoColor=black"/></a>
+          <a href='#'><img src="https://img.shields.io/badge/-React-61DAFB?style=flat-square&logo=React&logoColor=black"/></a>
+          <a href='#'><img src="https://img.shields.io/badge/-NodeJS-339933?style=flat-square&logo=Node.js&logoColor=white"/></a>
+          <a href='#'><img src='https://img.shields.io/badge/-Express.js-404D59?style=flat-square&for-the-badge'/></a>
+          </div>
+        </div>
       </div>
     </>
       
